b2d8a5f5a82a784041c07d4f9352f2d53ac6ed14
zhaiyongtao <501671900@qq.com>
2020-12-03 16:21:38 +0800

    【add】 了解react脚手架

---
+++ 04_know-about-react-cli/src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    render() {
+        // 引入React 是因为jsx语法会调用React.createElement方法
+        return (
+            <div>
+                Hello React!!
+            </div>
+        )
+    }
+}
+
+export default App;
