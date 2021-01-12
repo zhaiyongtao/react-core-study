f7aac2d72f1bfdfca8f98b5e2d58da9337911599
zhaiyongtao <501671900@qq.com>
2021-01-12 18:04:56 +0800

    [ADD]:子传父

---
+++ 05_learn_component/src/pages/06_SonPassValueToParent/index.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+
+
+class SonPassValueToParent extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <>
+                <h2>
+                    当前计数：
+                </h2>
+            </>
+        )
+    }
+}
+
+export default SonPassValueToParent;
--- 05_learn_component/src/pages/FatherAndSonConnect/ClassComponents/index.js
+++ 05_learn_component/src/pages/FatherAndSonConnect/ClassComponents/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types';
 
-class ChildCpn extends Component {
+class ChildCpn1 extends Component {
     render() {
         const {name, age, height} = this.props
         return (
@@ -10,19 +11,41 @@ class ChildCpn extends Component {
                 </h2>
                 <p>
                     展示父组件传递过来的数据：{
-                    name + "" + age + height
+                    name + "," + age +','+ height
                 }
                 </p>
             </div>
         )
     }
 }
+// 若果类型不匹配符，内容会正常展示，这个只是EsLint报错，并不影响程序执行，控制台会有警告输出
+ChildCpn1.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number.isRequired,
+    height: PropTypes.string,
+}
+ChildCpn1.defaultProps = {
+    name: 'mgy'
+}
+
+function ChildCpn2(props) {
+    const {name, age, height} = props;
+    return (
+        <div>
+            <h2>我是function组件</h2>
+            <p>
+                展示父组件传递过来的{name + ',' + age + ',' + height}
+            </p>
+        </div>
+    )
+}
 
 class ClassComponents extends Component {
     render() {
         return (
             <div>
-                <ChildCpn name="zyt" age="18" height="1.88"/>
+                <ChildCpn1 age="zyt" height="1.88"/>
+                <ChildCpn2 name="zyt2" age='17' height="1.78"/>
             </div>
         )
 
