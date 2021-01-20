16790ff940dff13b6ad8b6f9e1546085040b9bcd
zhaiyongtao <501671900@qq.com>
2021-01-20 18:34:43 +0800

    【FIX】:不可变数据的力量

---
+++ 05_learn_component/src/pages/08_SetStateMethod/NoChangedState.js
@@ -0,0 +1,22 @@
+/**
+ * NoChangedState$ - State的数据不可变性
+ * @Author: BuzzLightyear.Z
+ * @Email: 501671900@qq.com
+ * @Date: 2021/1/20 6:08 下午
+ * @LastEditTime: 2021/1/20 6:08 下午
+ */
+
+import React, {PureComponent} from 'react';
+
+class NoChangedState extends PureComponent {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default NoChangedState;
+
