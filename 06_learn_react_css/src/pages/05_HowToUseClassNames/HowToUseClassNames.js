1339c0e128a6d9456b9c367ee1c49529b19b8c5e
翟永涛 <yongtao.di@hand-china.com>
2021-02-18 23:46:29 +0800

    [ADD] 增加React Css 代码,并新建learn Antd项目

--- 06_learn_react_css/src/pages/05_HowToUseClassNames/HowToUseClassNames.js
+++ 06_learn_react_css/src/pages/05_HowToUseClassNames/HowToUseClassNames.js
@@ -1,5 +1,6 @@
 import React, {PureComponent} from 'react';
 import classNames from 'classnames';
+
 class HowToUseClassNames extends PureComponent {
     constructor(props) {
         super(props);
@@ -8,17 +9,23 @@ class HowToUseClassNames extends PureComponent {
             isActive: true
         }
     }
+
     render() {
         const {isActive} = this.state;
+        const errClass = "error";
+        const nullClass = null;
+        const undefinedClass = undefined;
         return (
             <div>
-                 {/*未使用classnames*/}
+                {/*未使用classnames*/}
                 {/*<h2 className="title active">我是标题1</h2>*/}
-                <h2 className={"title " + (isActive ? "active": "")}>我是标题1</h2>
+                <h2 className={"title " + (isActive ? "active" : "")}>我是标题1</h2>
                 {/*<h2 className="title active">我是标题2</h2>*/}
-                <h2 className={["title", (isActive ? "active": "")].join(" ")}>我是标题2</h2>
+                <h2 className={["title", (isActive ? "active" : "")].join(" ")}>我是标题2</h2>
                 {/*<h2 className="title active">我是标题3</h2>*/}
-                <h2 className={classNames("title","active")}>我是标题3</h2>
+                <h2 className={classNames("title", "active")}>我是标题3</h2>
+                <h2 className={classNames('foo', {bar: isActive})}>我是标题4</h2>
+                <h2 className={classNames('foo',errClass , nullClass,undefinedClass,{bar: isActive})}>我是标题4</h2>
             </div>
         );
     }
