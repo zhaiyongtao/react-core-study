fd2b89eaae6ed24f0ce61daa08776cf67e48cb03
zhaiyongtao <501671900@qq.com>
2021-03-15 18:38:57 +0800

    增加一些面试题代码

---
+++ 0_GrammarSupplement/test.js
@@ -0,0 +1,26 @@
+/**
+ * test$ - 面试题
+ * @Author: BuzzLightyear.Z
+ * @Email: 501671900@qq.com
+ * @Date: 2021/3/15 4:39 下午
+ * @LastEditTime: 2021/3/15 4:39 下午
+ */
+
+var x = 1;
+var output = (function () {
+  delete x;
+  return x;
+})();
+console.log(output);
+
+console.log(a);
+var a = 100;
+fn("zhangsan");
+function fn(name) {
+  age = 20;
+  console.log(name, age);
+  var age;
+  console.log(age);
+}
+console.log(b);
+b = 100;
